7b0ee558ac1de01fab185de2be188038a915372c
Jangwon <wkddnjset@gmail.com>
2021-11-16 23:32:30 +0900

    release: version 0.0.3

--- src/core.ts
+++ src/core.ts
@@ -1,32 +1,33 @@
-import { initProps } from './types';
+import { initProps } from "./types";
 
-const _url = '0auth.kr';
-// const _url = 'localhost:3000';
+const _url = "0auth.kr";
 
 let _init = false;
-const _option: initProps = {
-  brand: '',
+const _option = {
+  brand: "",
 };
 
-export const initialize = (option: initProps) => {
+export const initialize = (option) => {
   if (!_init) {
     _option.brand = option.brand;
     _init = true;
   }
   const urlParams = new URLSearchParams(window.location.search);
-  const access = urlParams.get('0auth_access');
-  const refresh = urlParams.get('0auth_refresh');
-  const next = urlParams.get('0auth_next');
+  const access = urlParams.get("0auth_access");
+  const refresh = urlParams.get("0auth_refresh");
+  const next = urlParams.get("0auth_next");
   if (access && refresh) {
-    localStorage.setItem('0auth_token', JSON.stringify({ access, refresh }));
-    window.location.replace(next ? next : '/');
+    localStorage.setItem("0auth_token", JSON.stringify({ access, refresh }));
+    window.location.replace(next ? next : "/");
   }
 };
 
 export const login = () => {
   if (_option.brand) {
-    window.location.replace(`//${_option.brand}.${_url}/?next=${window.location.href}`);
+    window.location.replace(
+      `//${_option.brand}.${_url}/?next=${window.location.href}`
+    );
   } else {
-    console.error('[Error] initialize app');
+    console.error("[Error] initialize app");
   }
 };
